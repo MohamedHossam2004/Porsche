761cacd9f8d0be59eedfe5bfef05e54057b5e840
Powerscore <alaa.ashraf.helmy@gmail.com>
2024-05-12 00:12:07 +0300

    front end testing

---
+++ client/src/Test/test.jsx
@@ -0,0 +1,53 @@
+
+
+function test(){
+    page =  
+    <div class="container-fluid">  
+        
+    <div class="d-flex">
+
+      <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 20vh; height: 100vh;">
+        <a href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+          <img src="https://w7.pngwing.com/pngs/909/687/png-transparent-hamburger-button-hot-dog-computer-icons-pancake-hot-dog-share-icon-navbar-menu-thumbnail.png" width="30" height="30" class="d-block align-top" alt=""/>
+          <span class="fs-4">Porsche</span>
+        </a>
+        <hr/>
+        <ul class="nav nav-pills flex-column mb-auto">
+          <li class="nav-item">
+            <a href="index.html" class="nav-link" aria-current="page">
+              Home
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="register.html" class="nav-link">
+              Register/Login
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="contact.html" class="nav-link">
+              Contact Us
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="about.html" class="nav-link active">
+              About
+            </a>
+          </li>
+        </ul>
+        
+      </div>
+
+      <section id="about" class="container mt-5">
+        <h2>About Porsche</h2>
+        <p>Porsche, a name synonymous with performance and luxury, boasts a rich heritage dating back to 1931. Founded by Ferdinand Porsche, the brand's mission has always been to push the boundaries of automotive engineering. From the iconic 911 to the groundbreaking Taycan electric sports car, Porsche has consistently delivered vehicles that redefine driving dynamics and technological innovation. Victories at legendary races like Le Mans and countless motorsport achievements stand as testaments to Porsche's relentless pursuit of excellence. This dedication to performance and innovation continues to inspire drivers and car enthusiasts worldwide.</p>
+      </section>
+
+    </div>
+
+            
+    </div>;
+    return page;
+
+
+}
+export default test;
